a3af1729d44141bc72301fbb0df6d3d717e42433
Aadyaghumre <83207391+Aadyaghumre@users.noreply.github.com>
2021-04-27 11:41:29 +0530

    Add files via upload

---
+++ Python Task sheet 1 - Task 1.py
@@ -0,0 +1,9 @@
+"""
+'My school'
+"""
+print("1. My school's name is J.G.R.V.K.")
+print("2. It is in Ramamurthynagar, in Bangalore")
+print("3. It is a big building with a ground, swimming pool, yoga hall, sports room.")
+print("4. It has 5 floors.")
+print("5. There are many teachers who teach us many interesting things.")
+print("6. My school is like a temple for me where all the gods bless us.")
