7c43f3ee8e68885a88cfef5f6cdaef2e02433407
Aadyaghumre <83207391+Aadyaghumre@users.noreply.github.com>
2021-04-27 11:58:46 +0530

    Add files via upload

---
+++ Python Task sheet 1 - Task 4.py
@@ -0,0 +1,13 @@
+"""
+Code to store 2 values in variables 'num1' and 'num2' and print the  remainder of 
+division of num1 and num2.
+"""
+
+num1 = 78
+num2 = 5
+
+#Use of Modular division.
+
+Modulo= num1 % num2
+
+print("The remainder of division of num1 and num2= " + str(Modulo))
