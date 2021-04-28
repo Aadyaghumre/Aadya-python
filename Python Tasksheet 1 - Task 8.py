ff9b077c9ab4c86cb136c5b8ed71d6d41f17b69b
Aadyaghumre <83207391+Aadyaghumre@users.noreply.github.com>
2021-04-28 12:45:41 +0530

    Add files via upload

---
+++ Python Tasksheet 1 - Task 8.py
@@ -0,0 +1,13 @@
+"""
+Program to find average of 5 numbers.
+"""
+
+num1 = float(input("Enter the 1st required number: "))
+num2 = float(input("Enter the 2nd required number: "))
+num3 = float(input("Enter the 3rd required number: "))
+num4 = float(input("Enter the 4th required number: "))
+num5 = float(input("Enter the 5th required number: "))
+
+Avg = (num1 + num2 + num3 + num4 + num5)/5
+
+print("The average of the numbers is: " +  str(Avg))
