039847d8cb18e79fbbc339496c8a32cfe2cd3e9e
Aadyaghumre <83207391+Aadyaghumre@users.noreply.github.com>
2021-04-28 11:28:59 +0530

    Add files via upload

---
+++ Python Tasksheet 1 - Task 6.py
@@ -0,0 +1,9 @@
+"""
+Code which takes input as 'city' name from the user and prints 'city
+name' is an awesome place.
+"""
+
+city = input("Enter the city name: ")
+
+print(city + " is an awesome place.")
+
