b589a8971ba7e28d073886db556853bd6e26530a
Aadyaghumre <83207391+Aadyaghumre@users.noreply.github.com>
2021-04-29 11:14:33 +0530

    Add files via upload

---
+++ Task sheet 2 - Task 2.py
@@ -0,0 +1,15 @@
+"""
+Program which generates auto replies according to the answer by the user to the questions:
+What is your name?
+You are in which school?
+What is your hobby?
+"""
+
+q1 = input("What is your name? Ans: ")
+print("Hello "+q1+ "!")
+
+q2 = input("You are from which school? Ans: ")
+print(q2+ " is a renowned school in the city!")
+
+q3 = input("What is your hobby? Ans: ")
+print("Wow! "+q3 + " is a unique hobby.")
