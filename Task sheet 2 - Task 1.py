66a2a0587f276ab6ad0f673c031ea2fbf1a04d87
Aadyaghumre <83207391+Aadyaghumre@users.noreply.github.com>
2021-04-29 11:02:35 +0530

    Add files via upload

---
+++ Task sheet 2 - Task 1.py
@@ -0,0 +1,9 @@
+"""
+Program to convert quantity entered by user in pounds into kg.
+"""
+
+weight= float(input("Enter weight in pounds: "))
+
+kg = weight * 0.453592
+
+print(str(weight)+" pound(s)"+ " is = "+str(kg)+" kg.")
